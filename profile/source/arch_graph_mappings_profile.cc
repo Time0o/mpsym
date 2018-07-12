525789646b171cb0f8edcca98cab268fc5a6748d
Timo Nicolai <timonicolai@arcor.de>
2018-07-12 14:24:23 +0200

    Print execution time during profiling

--- profile/source/arch_graph_mappings_profile.cc
+++ profile/source/arch_graph_mappings_profile.cc
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
@@ -13,7 +14,7 @@
 using cgtl::ArchGraph;
 using cgtl::TaskMapping;
 
-static void compare_mappings(ArchGraph const &ag, std::size_t num_tasks)
+static long compare_mappings(ArchGraph const &ag, std::size_t num_tasks)
 {
   static std::default_random_engine re(time(nullptr));
 
@@ -22,18 +23,27 @@ static void compare_mappings(ArchGraph const &ag, std::size_t num_tasks)
   std::vector<std::size_t> task_mapping1(num_tasks);
   std::vector<std::size_t> task_mapping2(num_tasks);
 
+  long exec_ticks = 0;
+
   for (int run = 0; run < RUNS; ++run) {
     for (std::size_t i = 0u; i < num_tasks; ++i) {
       task_mapping1[i] = dist(re);
       task_mapping2[i] = dist(re);
     }
 
+    auto t0 = std::chrono::high_resolution_clock::now();
+
     TaskMapping tm1 = ag.mapping(task_mapping1);
     TaskMapping tm2 = ag.mapping(task_mapping2);
 
     volatile bool result = ag.equivalent(tm1, tm2);
     (void) result;
+
+    exec_ticks += std::chrono::duration_cast<std::chrono::microseconds>(
+      std::chrono::high_resolution_clock::now() - t0).count();
   }
+
+  return exec_ticks;
 }
 
 int main()
@@ -44,6 +54,8 @@ int main()
     resource_path("mcsoc.lua")
   };
 
+  long exec_ticks = 0;
+
   for (auto const &path : arch_graphs) {
     ag.fromlua(path);
     ag.generate_automorphisms();
@@ -53,7 +65,9 @@ int main()
       printf("Comparing mappings (%zu tasks on %zu processors, %d mappings)\n",
              num_tasks, ag.num_processors(), RUNS);
 
-      compare_mappings(ag, num_tasks);
+      exec_ticks += compare_mappings(ag, num_tasks);
     }
   }
+
+  printf("Total execution time: %ld microseconds\n", exec_ticks);
 }
