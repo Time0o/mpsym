4aa9a0e7d3dc5fb3d60e4d16376c38d3fa0555ca
Timo Nicolai <timonicolai@arcor.de>
2018-09-02 13:20:12 +0200

    Add expand_partition utility method

--- include/util.h
+++ include/util.h
@@ -1,6 +1,9 @@
 #ifndef _GUARD_UTIL_H
 #define _GUARD_UTIL_H
 
+#include <algorithm>
+#include <vector>
+
 namespace cgtl
 {
 
@@ -28,6 +31,23 @@ T factorial(T x)
   T res = 1u;
   while (x > 1u)
     res *= x--;
+
+  return res;
+}
+
+template<typename T>
+std::vector<std::vector<T>> expand_partition(std::vector<T> set)
+{
+  auto mm = std::minmax_element(set.begin(), set.end());
+  auto first = *mm.first;
+  auto last = *mm.second;
+
+  std::vector<std::vector<T>> res(last - first + 1);
+  for (auto x = 0u; x < set.size(); ++x)
+    res[set[x] - first].push_back(x);
+
+  std::sort(res.begin(), res.end(),
+           [](std::vector<T> a, std::vector<T> b){ return a[0] < b[0]; });
 
   return res;
 }
