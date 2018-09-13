89cd35ed5b1ed8ea7d1c8f0224e15320ffa745ec
Timo Nicolai <timonicolai@arcor.de>
2018-09-13 20:44:20 +0200

    Add Perm documentation

--- include/perm.h
+++ include/perm.h
@@ -1,3 +1,10 @@
+/*
+ * @file perm.h
+ * @brief Defines `Perm`.
+ *
+ * @author Timo Nicolai
+ */
+
 #ifndef _GUARD_PERM_H
 #define _GUARD_PERM_H
 
@@ -34,26 +41,179 @@ struct hash<cgtl::PermWord>
 namespace cgtl
 {
 
+/** A permutation representation.
+ *
+ * The `Perm` class represents a permutation on the set \f$X = \{1, \dots,
+ * n\}\f$ of positive integers (with an arbitrary fixed \f$n\f$), i.e. a
+ * bijective mapping from \f$X\f$ to itself. It realizes common operations like
+ * permutation application and chaining via convenient operator overloads.
+ * Only permutations on continuous ranges of positive integers starting from
+ * \f$1\f$ are supported, i.e. a permutation represented by a `Perm` object
+ * always has a domain of the form \f$\{1, 2, 3, \dots, n\}\f$ with \f$ n \in
+ * \mathbb{N}_+\f$.
+ */
 class Perm
 {
 friend std::size_t std::hash<Perm>::operator()(Perm const &perm) const;
 
 public:
+  /** Construct an *identity permutation*.
+   *
+   * An identity permutation maps every element in its domain to itself.
+   *
+   * \param degree
+   *	 determines the domain of the resulting permutation (which is \f$\{1,
+   *	 \dots, degree\}\f$). For `degree == 0u` the permutation' domain is the
+   *	 empty set (i.e. using `operator[]` with any index is invalid in this
+   *	 case and multiplication with any other permutation will yield another
+   *	 permutation over the empty set)
+   */
   Perm(unsigned degree = 1);
-  Perm(std::vector<unsigned> const &perm);
-  Perm(unsigned n, std::vector<std::vector<unsigned>> const &cycles);
 
-  unsigned const& operator[](unsigned const i) const;
+  /** Construct a permutation from an explicit mapping description.
+   *
+   * \param perm
+   *     a vector of element images, if the image at the \f$i\f$th index has
+   *     the value \f$j\f$, the resulting permutation will map \f$i + 1\f$ to
+   *     \f$j\f$; all element images must thus be greater than `0u` and
+   *     different from each other in order to describe a valid permutation,
+   *     otherwise this constructor's behaviour is undefined
+   */
+  Perm(std::vector<unsigned> const &perm);
+
+  /** Construct a permutation from a permutation description given as product of
+   *  cycles.
+   *
+   * This constructor operates in accordance with the commonly encountered
+   * notation of permutations as products of disjoint cycles in mathematical
+   * literature (although here the cycles need not be disjoint, in this case
+   * the cycles are "chained together left to right" in accordance with
+   * `operator*=`). Note that this is mostly a convenience constructor which
+   * will almost always be slower than an equivalent call to
+   * Perm(std::vector<unsigned> const &perm).
+   *
+   * \param degree
+   *	 determines the domain of the resulting permutation (\f$\{1, \dots,
+   *	 degree\}\f$), which can not be unambiguously determined from the given
+   *	 `cycles`
+   *
+   * \param cycles
+   *     a cycle description of the resulting permutation, e.g. `{{1, 2}, {3,
+   *     4}}` to describe the permutation \f$(1 2)(3 4)\f$ (assuming `degree ==
+   *     4u`); if any cycle element is greater than `degree` this constructor's
+   *     behaviour is undefined
+   */
+  Perm(unsigned degree, std::vector<std::vector<unsigned>> const &cycles);
+
+  /** Apply a permutation.
+   *
+   * The result of invoking `operator[x]` on a permutation \f$p\f$ is the
+   * positive integer \f$y\f$ such that \f$y = p(x)\f$. If \f$x \notin dom(p)\f$
+   * this function's behaviour is undefined.
+   *
+   * \param x the positive integer on which to apply this permutation
+   *
+   * \return the positive integer `y` to which `x` is mapped by this permutation
+   */
+  unsigned const& operator[](unsigned const x) const;
+
+  /** Construct the inverse of a permutation.
+   *
+   * A permutation \f$p\f$'s inverse \f$p^{-1}\f$ is the permutation which maps
+   * every element \f$y \in im(p)\f$ to \f$x \in dom(p)\f$ if \f$y = p(x)\f$.
+   *
+   * \return a newly constructed `Perm` object inverse to this one
+   */
   Perm operator~() const;
+
+  /** Check whether two permutations are equal.
+   *
+   * Two permutations are equal by definition if they map all elements in their
+   * domains (which must be equal) to the same elements.
+   *
+   * \param rhs
+   *     the permutation which is to be compared with this one, if `rhs`'s
+   *     degree() is not equal to this permutation's degree(), this operator's
+   *     behaviour is undefined
+   *
+   * \return `true` if `rhs` is equal to this permutation according to the
+   *         definition above
+   */
   bool operator==(Perm const &rhs) const;
+
+  /** Check whether two permutations are not equal.
+   *
+   * This function's result is always equal to `!(*this == rhs)`.
+   *
+   * \param rhs
+   *     the permutation which is to be compared with this one, if `rhs`'s
+   *     degree() is not equal to this permutation's degree(), this operator's
+   *     behaviour is undefined
+   *
+   * \return `true` if `rhs` is not equal (see `operator==`) to this permutation
+   */
   bool operator!=(Perm const &rhs) const;
+
+  /** "Chain" another permutation to an existing permutation.
+   *
+   * After invoking `p1 *= p2` for `Perm` objects `p1` and `p2`, `p1` is the
+   * permutation which maps every element \f$x \in dom(p_1)\f$ to
+   * \f$p2(p1(x))\f$.
+   *
+   * \param rhs the permutation which is to be "chained" to this one
+   *
+   * \return a reference to this permutation
+   */
   Perm& operator*=(Perm const &rhs);
 
+  /** Obtain a permutation's *degree*.
+   *
+   * Assuming a permutation's domain is the set \f$\{1, \dots, n\}\f$, its
+   * degree is the positive integer \f$n\f$
+   *
+   * \return this permutation's degree
+   */
   unsigned degree() const { return _n; }
+
+  /** Check whether a permutation is an *identity permutation*.
+   *
+   * An identity permutation maps every element in its domain to itself.
+   *
+   * \return `true` if this permutation is an identity, else `false`
+   */
   bool id() const;
 
+  /** *Shift* a permutation's domain.
+   *
+   * This function constructs a new permutation \f$p'\f$ from this permutation,
+   * \f$p\f$, with the domain \f$\{1, \dots, high - low + 1\}\f$ whose action
+   * is defined by \f$p'(i) = j \iff p(i + low) = j + low\f$ for \f$1 \leq i
+   * \leq high - low + 1\f$.
+   *
+   * \param low
+   *     the low end of the domain range which is to be "shifted downwards" as
+   *     described above
+   *
+   * \param high
+   *     the high end of the domain range which is to be "shifted downwards" as
+   *     described above
+   */
   Perm shifted(unsigned low, unsigned high) const;
 
+  /** Construct a *restricted* version of a permutation.
+   *
+   * "Restricting" a permutation to a new domain \f$d_{new}\f$ conceptually has
+   * the same effect as removing all cycles from the permutation's disjoint
+   * cycle representation which contain at least one element \f$x \notin
+   * d_{new}\f$.
+   *
+   * \param domain the domain to which to restrict the resulting permutation
+   *
+   * \param[out] id TODO: remove this parameter
+   *
+   * \return a restricted version of this permutation according to the
+   *         definition above
+   */
   Perm restricted(
     std::vector<unsigned> const &domain, bool *id = nullptr) const;
 
