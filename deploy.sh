b4e2d562b579aea1447be869f5e4112740714a20
Timo Nicolai <timonicolai@arcor.de>
2018-09-19 21:00:17 +0200

    Fix travis documentation deployment

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+USER_NAME="Time0o"
+REPO_NAME="TUD_computational_group_theory"
+REPO_URL="github.com/$USER_NAME/$REPO_NAME.git"
+
 if [ "${MATRIX_EVAL}" == "CC=gcc-7 && CXX=g++-7" ]; then
   # upload coverage data
   echo "Uploading coverage data..."
@@ -13,5 +17,24 @@ if [ "${MATRIX_EVAL}" == "CC=gcc-7 && CXX=g++-7" ]; then
   echo "Deploying documentation..."
   cmake -DCMAKE_BUILD_TYPE=Release ..
   make doxygen
-  bash "${TRAVIS_BUILD_DIR}"/deploy_documentation.sh
+
+  echo "Cloning gh-pages branch..."
+  git clone -b gh-pages "https://$REPO_URL"
+  cd "$REPO_NAME"
+
+  echo "Configuring git..."
+  git config push.default simple
+  git config user.name "Timo Nicolai"
+  git config user.name "timonicolai@arcor.de"
+
+  if [ -d ../doxygen/html ] && [ -f ../doxygen/html/index.html ]; then
+    echo "Pushing documentation..."
+    cp -r ../doxygen/html/* .
+    git add .
+    git commit --amend -m "Deploy documentation, current commit is $TRAVIS_COMMIT"
+    git push --force "https://$REPO_TOKEN@$REPO_URL" > /dev/null 2>&1
+  else
+    echo "Failed to find generated documentation" >&2
+    exit 1
+  fi
 fi
