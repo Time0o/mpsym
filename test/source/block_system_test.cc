06dfe9689c33e9425077b3c59e289dd46d0afe5d
Timo Nicolai <timonicolai@arcor.de>
2019-11-08 16:10:10 +0100

    Remove superfluous line in block_system_test.cc

--- test/source/block_system_test.cc
+++ test/source/block_system_test.cc
@@ -78,8 +78,6 @@ TEST(BlockSystemTest, CanFindMinimalBlockSystem)
     Perm(6, {{1, 2, 3, 4, 5, 6}}), Perm(6, {{2, 6}, {3, 5}})
   };
 
-  BlockSystem bs = BlockSystem::minimal(generators, {1, 3});
-
   EXPECT_TRUE(block_system_equal({{1, 3, 5}, {2, 4, 6}},
                                  BlockSystem::minimal(generators, {1, 3})))
     << "Minimal blocksystem correctly determined.";
