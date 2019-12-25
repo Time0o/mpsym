153905843e78d9a92db562ba46e66e7094aa8461
Timo Nicolai <timonicolai@arcor.de>
2019-12-25 17:42:43 +0100

    Clean up perm_set.h

--- include/perm_set.h
+++ include/perm_set.h
@@ -29,43 +29,52 @@ public:
 
   template<typename IT>
   PermSet(IT b, IT e)
-  {
-    insert(b, e);
-  }
+  { insert(b, e); }
 
-  unsigned degree() const {
+  unsigned degree() const
+  {
     assert(!empty() && "degree of empty permutation set not defined");
     return _perms[0].degree();
   }
 
-  bool empty() const { return _perms.empty(); }
-  unsigned size() const { return _perms.size(); }
+  bool empty() const
+  { return _perms.empty(); }
 
-  Perm const& operator[](unsigned i) const {
-    assert(i < size());
-    return _perms[i];
-  };
+  unsigned size() const
+  { return _perms.size(); }
 
-  Perm & operator[](unsigned i) {
+  Perm const& operator[](unsigned i) const
+  {
     assert(i < size());
     return _perms[i];
   }
 
-  const_iterator begin() const { return _perms.begin(); }
-  const_iterator end() const { return _perms.end(); }
+  Perm & operator[](unsigned i)
+  {
+    assert(i < size());
+    return _perms[i];
+  }
+
+  const_iterator begin() const
+  { return _perms.begin(); }
+
+  const_iterator end() const
+  { return _perms.end(); }
 
   void insert(Perm const &perm) {
     assert_degree(perm.degree());
     _perms.push_back(perm);
   }
 
-  void insert(Perm &&perm) {
+  void insert(Perm &&perm)
+  {
     assert_degree(perm.degree());
     _perms.emplace_back(perm); // TODO: forward
   }
 
   template<typename IT>
-  void insert(IT b, IT e) {
+  void insert(IT b, IT e)
+  {
 #ifndef NDEBUG
     for (auto it = b; it != e; ++it)
       assert_degree(it->degree());
@@ -74,12 +83,14 @@ public:
   }
 
   template<typename ...ARGS>
-  void emplace(ARGS &&...args) {
+  void emplace(ARGS &&...args)
+  {
     _perms.emplace_back(args...); // TODO: forward
     assert_degree(_perms.back().degree());
   }
 
-  size_type erase(Perm const &perm) {
+  size_type erase(Perm const &perm)
+  {
     size_type removed = 0u;
 
     auto it = _perms.begin();
@@ -96,19 +107,21 @@ public:
   }
 
   template<typename IT>
-  IT erase(IT it) { return _perms.erase(it); }
+  IT erase(IT it)
+  { return _perms.erase(it); }
 
-  void clear() { _perms.clear(); }
+  void clear()
+  { _perms.clear(); }
 
   void make_unique();
 
   void minimize_degree();
 
-  void assert_not_empty() const {
-    assert(!empty() && "permutation set not empty");
-  }
+  void assert_not_empty() const
+  { assert(!empty() && "permutation set not empty"); }
 
-  void assert_degree(unsigned deg) const {
+  void assert_degree(unsigned deg) const
+  {
 #ifndef NDEBUG
     assert((empty() || degree() == deg) && "permutations have correct degree");
 #else
