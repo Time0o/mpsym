66a40e0c3b377625966607184453b3db0f6fee37
Timo Nicolai <timonicolai@arcor.de>
2020-03-30 19:21:19 +0200

    Make Exynos example run with GAP

--- resources/resources/exynos.lua
+++ resources/resources/exynos.lua
@@ -5,10 +5,14 @@ local p1_processors = mpsym.append_processors(processors, mpsym.identical_proces
 local p2_processors = mpsym.append_processors(processors, mpsym.identical_processors(4, 'P2'))
 
 local channels = {}
-mpsym.append_channels(channels, mpsym.self_connected_channels(processors, 'L1'))
-mpsym.append_channels(channels, mpsym.self_connected_channels(p1_processors, 'L2'))
-mpsym.append_channels(channels, mpsym.self_connected_channels(p2_processors, 'L2'))
-mpsym.append_channels(channels, mpsym.fully_connected_channels(processors, 'RAM'))
+--mpsym.append_channels(channels, mpsym.self_connected_channels(processors, 'L1'))
+--mpsym.append_channels(channels, mpsym.self_connected_channels(p1_processors, 'L2'))
+--mpsym.append_channels(channels, mpsym.self_connected_channels(p2_processors, 'L2'))
+--mpsym.append_channels(channels, mpsym.fully_connected_channels(processors, 'RAM'))
+mpsym.append_channels(channels, mpsym.self_connected_channels(processors, 'C'))
+mpsym.append_channels(channels, mpsym.self_connected_channels(p1_processors, 'C'))
+mpsym.append_channels(channels, mpsym.self_connected_channels(p2_processors, 'C'))
+mpsym.append_channels(channels, mpsym.fully_connected_channels(processors, 'C'))
 
 return mpsym.ArchGraph:create{
   processors = processors,
