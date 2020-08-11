b50982b6c5f2fbb5cc2348e759262e81e84e54b0
Timo Nicolai <timonicolai@arcor.de>
2020-08-11 15:37:04 +0200

    Compile mpsym as shared object

--- cmake/GetNauty.cmake
+++ cmake/GetNauty.cmake
@@ -4,13 +4,15 @@ project(nauty-download NONE)
 
 include(ExternalProject)
 
+set(EXTRA_CFLAGS "'-O3 -fPIC'")
 set(NCPU 12)
+
 ExternalProject_Add(nauty
   URL               http://pallini.di.uniroma1.it/nauty26r10.tar.gz
   SOURCE_DIR        "${NAUTY_WORKDIR}"
   BINARY_DIR        "${NAUTY_WORKDIR}"
-  CONFIGURE_COMMAND "${NAUTY_WORKDIR}/configure"
-  BUILD_COMMAND     make -C "${NAUTY_WORKDIR}" nauty.a -j "${NCPU}"
+  CONFIGURE_COMMAND ${NAUTY_WORKDIR}/configure CFLAGS=${EXTRA_CFLAGS}
+  BUILD_COMMAND     ""
   INSTALL_COMMAND   ""
   TEST_COMMAND      ""
 )
