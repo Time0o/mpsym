c3056a017c2a156047870ba60e09571dfa31188a
Timo Nicolai <timonicolai@arcor.de>
2020-08-13 11:55:05 +0200

    Synchronize versions

--- python/mpsym.cpp
+++ python/mpsym.cpp
@@ -5,7 +5,8 @@ namespace py = pybind11;
 int add(int a, int b) { return a + b; }
 
 PYBIND11_MODULE(mpsym, m) {
-  m.doc() = "MPSoC symmetry reduction package";
+  m.doc() = "MPSoC Symmetry Reduction";
+  m.attr("__version__") = "1.0";
 
   m.def("add", &add, "Add two numbers"); // TODO
 }
