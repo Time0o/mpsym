de0c142dca995b3402212db67e6145a6e6b7aa7f
Timo Nicolai <timonicolai@arcor.de>
2020-08-13 18:29:06 +0200

    Remove resources/

--- resources/resources/parallella.lua
+++
@@ -1,9 +0,0 @@
-local mpsym = require 'mpsym'
-
-local processors = mpsym.identical_processors(64, 'P')
-local channels = mpsym.grid_channels(processors, 'C')
-
-return mpsym.ArchGraph:create{
-  processors = processors,
-  channels = channels
-}
