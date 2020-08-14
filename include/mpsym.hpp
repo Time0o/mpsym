c7782cb3e392b1035e26889a227b657639ccc72e
Timo Nicolai <timonicolai@arcor.de>
2020-08-14 13:24:40 +0200

    Add missing mpsym.hpp

---
+++ include/mpsym.hpp
@@ -0,0 +1,11 @@
+#ifndef GUARD_MPSYM_H
+#define GUARD_MPSYM_H
+
+#include "arch_graph.hpp"
+#include "arch_graph_cluster.hpp"
+#include "arch_graph_system.hpp"
+#include "arch_uniform_super_graph.hpp"
+#include "task_mapping.hpp"
+#include "task_orbits.hpp"
+
+#endif // GUARD_MPSYM_H
