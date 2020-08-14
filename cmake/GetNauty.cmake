339e940505dc59c4fef237164a5ec1376d3628e3
Timo Nicolai <timonicolai@arcor.de>
2020-08-14 12:20:02 +0200

    make install

--- cmake/GetNauty.cmake
+++ cmake/GetNauty.cmake
@@ -5,12 +5,13 @@ project(nauty-download NONE)
 include(ExternalProject)
 
 set(NCPU 12)
-ExternalProject_Add(nauty
+ExternalProject_Add(nauty_traces
   URL               http://pallini.di.uniroma1.it/nauty26r10.tar.gz
   SOURCE_DIR        "${NAUTY_WORKDIR}"
   BINARY_DIR        "${NAUTY_WORKDIR}"
   CONFIGURE_COMMAND "${NAUTY_WORKDIR}/configure"
-  BUILD_COMMAND     make -C "${NAUTY_WORKDIR}" nauty.a -j "${NCPU}"
+  BUILD_COMMAND     make -C "${NAUTY_WORKDIR}" nauty.a -j "${NCPU}" &&
+                    mv "${NAUTY_WORKDIR}/nauty.a" "${NAUTY_LIB}"
   INSTALL_COMMAND   ""
   TEST_COMMAND      ""
 )
