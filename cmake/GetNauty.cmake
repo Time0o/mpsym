ddd586272de56f485eb34c61851eb1a9f7264f7b
Timo Nicolai <timonicolai@arcor.de>
2020-08-15 17:09:34 +0200

    Build shared library

--- cmake/GetNauty.cmake
+++ cmake/GetNauty.cmake
@@ -4,15 +4,15 @@ project(nauty-download NONE)
 
 include(ExternalProject)
 
+set(EXTRA_CFLAGS "'-O3 -fPIC'")
 set(NCPU 4)
 
 ExternalProject_Add(nauty_traces
   URL               http://pallini.di.uniroma1.it/nauty26r10.tar.gz
   SOURCE_DIR        "${NAUTY_WORK_DIR}"
   BINARY_DIR        "${NAUTY_WORK_DIR}"
-  CONFIGURE_COMMAND "${NAUTY_WORK_DIR}/configure"
-  BUILD_COMMAND     make -C "${NAUTY_WORK_DIR}" nauty.a -j "${NCPU}" &&
-                    mv "${NAUTY_WORK_DIR}/nauty.a" "${NAUTY_LIB}"
+  CONFIGURE_COMMAND "${NAUTY_WORK_DIR}/configure" "CFLAGS=${EXTRA_CFLAGS}"
+  BUILD_COMMAND     ""
   INSTALL_COMMAND   ""
   TEST_COMMAND      ""
 )
