24acd53cb16d036315c095a09a3a1372dd305133
Timo Nicolai <timonicolai@arcor.de>
2020-08-27 10:59:50 +0200

    pympsym: fix module name

--- python/pympsym.cpp
+++ python/pympsym.cpp
@@ -120,7 +120,7 @@ struct type_caster<mp::cpp_int> {
 
 #define PYBIND11_MODULE_(name, m) PYBIND11_MODULE(name, m)
 
-PYBIND11_MODULE_(PROJECT_NAME, m)
+PYBIND11_MODULE_(PYTHON_MODULE, m)
 {
   m.doc() = DESCRIPTION;
   m.attr("__version__") = VERSION;
