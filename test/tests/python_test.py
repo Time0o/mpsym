988a15c6f609313f8c453010e1b16924832572e9
Timo Nicolai <timonicolai@arcor.de>
2020-09-07 17:02:27 +0200

    pympsym: add from_nauty, to/from_json tests

--- test/tests/python_test.py
+++ test/tests/python_test.py
@@ -187,6 +187,28 @@ class ArchGraphSystemTest(unittest.TestCase):
         for orbit in [self.ag_orbit1, self.ag_orbit2]:
             self.assertEqual(self.ag.orbit(orbit[0]), orbit)
 
+    def test_from_nauty(self):
+        vertices_super = 4
+        adj_super = {0: [1], 1: [2], 2: [3]}
+
+        vertices_proto = 16
+        adj_proto = {0:  [1,4],  1:  [2,5],   2:  [3,6],   3:  [7],
+                     4:  [5,8],  5:  [6,9],   6:  [7,10],  7:  [11],
+                     8:  [9,12], 9:  [10,13], 10: [11,14], 11: [15],
+                     12: [13],   13: [14],    14: [15]}
+
+        ag_super = mp.ArchGraphSystem.from_nauty(vertices_super, adj_super, directed=False)
+        ag_proto = mp.ArchGraphSystem.from_nauty(vertices_proto, adj_proto, directed=False)
+
+        haec_nauty = mp.ArchUniformSuperGraph(ag_super, ag_proto)
+
+        self.assertEqual(haec_nauty.automorphisms(), self.ag.automorphisms())
+
+    def test_to_from_json(self):
+        json = self.ag.to_json()
+        ag_from_json = mp.ArchGraphSystem.from_json(json)
+        self.assertEqual(ag_from_json.automorphisms(), self.ag.automorphisms())
+
 
 if __name__ == '__main__':
     unittest.main()
