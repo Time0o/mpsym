3e620a9ffe64b749632d04f64f7ba185dd768e00
Timo Nicolai <timonicolai@arcor.de>
2020-10-24 21:43:30 +0200

    Add timeout.hpp

---
+++ include/timeout.hpp
@@ -0,0 +1,113 @@
+#ifndef GUARD_TIMEOUT_H
+#define GUARD_TIMEOUT_H
+
+#include <chrono>
+#include <functional>
+#include <future>
+#include <stdexcept>
+#include <thread>
+#include <utility>
+
+#include <boost/optional.hpp>
+
+namespace mpsym
+{
+
+namespace internal
+{
+
+namespace timeout
+{
+
+struct TimeoutError : public std::runtime_error
+{
+  TimeoutError(std::string const &what)
+  : std::runtime_error(what + " timeout")
+  {}
+};
+
+struct AbortedError : public std::runtime_error
+{
+  AbortedError(std::string const &what)
+  : std::runtime_error(what + " aborted")
+  {}
+};
+
+template<typename FUNC, typename ...ARGS>
+using ReturnType = decltype(std::declval<FUNC>()(std::declval<ARGS>()...));
+
+template<typename FUNC, typename ...ARGS>
+using ReturnTypeWrapper = boost::optional<ReturnType<FUNC, ARGS...>>;
+
+template<typename FUNC, typename ...ARGS, typename REP, typename PERIOD>
+std::future<ReturnType<FUNC, ARGS...>> future_with_timeout(
+  std::string const &what,
+  std::chrono::duration<REP, PERIOD> const &timeout,
+  FUNC &&f,
+  ARGS &&...args)
+{
+  std::packaged_task<ReturnType<FUNC, ARGS...>()> task(
+   [&]() { return f(std::forward<ARGS>(args)...); });
+
+  auto future(task.get_future());
+
+  std::thread thread(std::move(task));
+
+  if (future.wait_for(timeout) == std::future_status::timeout) {
+     thread.detach();
+     throw TimeoutError(what);
+  } else {
+     thread.join();
+     return future;
+  }
+}
+
+template<typename FUNC, typename ...ARGS, typename REP, typename PERIOD>
+typename std::enable_if<std::is_void<ReturnType<FUNC, ARGS...>>::value>::type
+run_with_timeout(std::string const &what,
+                 std::chrono::duration<REP, PERIOD> const &timeout,
+                 FUNC &&f,
+                 ARGS &&...args)
+{
+  (void)future_with_timeout(
+    what,
+    timeout,
+    [&](ARGS &&...args) {
+       try {
+         f(std::forward<ARGS>(args)...);
+       } catch (AbortedError const &aborted) {}
+     },
+     std::forward<ARGS>(args)...);
+}
+
+template<typename FUNC, typename ...ARGS, typename REP, typename PERIOD>
+typename std::enable_if<!std::is_void<ReturnType<FUNC, ARGS...>>::value,
+                        ReturnType<FUNC, ARGS...>>::type
+run_with_timeout(std::string const &what,
+                 std::chrono::duration<REP, PERIOD> const &timeout,
+                 FUNC &&f,
+                 ARGS &&...args)
+{
+  auto future(future_with_timeout(
+    what,
+    timeout,
+    [&](ARGS &&...args) {
+      try {
+        return ReturnTypeWrapper<FUNC, ARGS...>(
+          f(std::forward<ARGS>(args)...));
+      } catch (AbortedError const &aborted) {
+        return ReturnTypeWrapper<FUNC, ARGS...>();
+      }
+    },
+    std::forward<ARGS>(args)...));
+
+  return *future.get();
+}
+
+} // namespace timeout
+
+} // namespace internal
+
+} // namespace mpsym
+
+#endif // GUARD_TIMEOUT_H
---
+++ test/tests/timeout_test.cpp
@@ -0,0 +1,82 @@
+#include <atomic>
+#include <chrono>
+#include <thread>
+
+#include "gmock/gmock.h"
+
+#include "timeout.hpp"
+
+#include "test_main.cpp"
+
+using namespace mpsym::internal::timeout;
+
+using ms = std::chrono::milliseconds;
+
+namespace
+{
+
+template<typename REP, typename PERIOD>
+void sleep(std::chrono::duration<REP, PERIOD> const &time)
+{ std::this_thread::sleep_for(time); }
+
+TEST(TimeoutTest, CanTimeoutFunction)
+{
+  enum : int { ARG = 42 };
+
+  auto id_no_timeout = [](int arg)
+  {
+    sleep(ms(10));
+    return arg;
+  };
+
+  EXPECT_EQ(ARG, run_with_timeout("id_no_timeout", ms(100), id_no_timeout, ARG))
+    << "Function returns before timeout.";
+
+  auto id_timeout = [](int arg)
+  {
+    sleep(ms(1000));
+    return arg;
+  };
+
+  EXPECT_THAT(
+    [&](){ run_with_timeout("id_timeout", ms(100), id_timeout, ARG); },
+    testing::ThrowsMessage<TimeoutError>("id_timeout timeout"))
+      << "Function timeout yields exception.";
+
+  auto endless_loop = [](std::atomic<bool> &abort, std::atomic<bool> &done)
+  {
+    for (;;) {
+      if (abort.load())
+        break;
+
+      sleep(ms(10));
+    }
+
+    done.store(true);
+
+    throw AbortedError("endless_loop_abort");
+  };
+
+  std::atomic<bool> endless_loop_abort(false);
+  std::atomic<bool> endless_loop_done(false);
+
+  EXPECT_THAT(
+    [&]() {
+      run_with_timeout("endless_loop",
+                       ms(100),
+                       endless_loop,
+                       endless_loop_abort,
+                       endless_loop_done);
+    },
+    testing::ThrowsMessage<TimeoutError>("endless_loop timeout"))
+      << "Function timeout yields exception.";
+
+  endless_loop_abort.store(true);
+
+  sleep(ms(100));
+
+  EXPECT_TRUE(endless_loop_done.load())
+    << "Timed out thread terminates execution after abort flag is set.";
+}
+
+} // anonymous namespace
