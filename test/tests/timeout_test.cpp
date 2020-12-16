dab4eb6280327fc6d18b57b1af2fef013ae64511
Timo Nicolai <timonicolai@arcor.de>
2020-12-16 15:52:46 +0100

    timeout_test: get rid of ThrowsMessage

--- test/tests/timeout_test.cpp
+++ test/tests/timeout_test.cpp
@@ -13,10 +13,22 @@
 
 using namespace mpsym::internal::timeout;
 
-using ms = std::chrono::milliseconds;
+#define EXPECT_THROW_WITH_MESSAGE(statement, \
+                                  exception_type, \
+                                  exception_what) \
+  EXPECT_THROW( \
+    try { \
+      statement; \
+    } catch (const exception_type& e) { \
+      EXPECT_STREQ(exception_what, e.what()); \
+      throw; \
+    }, \
+    exception_type)
 
 namespace
 {
+
+using ms = std::chrono::milliseconds;
 
 template<typename REP, typename PERIOD>
 void sleep(std::chrono::duration<REP, PERIOD> const &time)
@@ -32,7 +44,10 @@ TEST(TimeoutTest, CanTimeoutFunction)
     return arg;
   };
 
-  EXPECT_EQ(ARG, run_with_timeout("id_no_timeout", ms(100), [&]{ return id_no_timeout(ARG); }))
+  EXPECT_EQ(ARG,
+            run_with_timeout("id_no_timeout",
+                             ms(100),
+                             [&]{ return id_no_timeout(ARG); }))
     << "Function returns before timeout.";
 
   auto id_timeout = [](int arg)
@@ -41,9 +56,12 @@ TEST(TimeoutTest, CanTimeoutFunction)
     return arg;
   };
 
-  EXPECT_THAT(
-    [&](){ run_with_timeout("id_timeout", ms(100), [&]{ return id_timeout(ARG); }); },
-    testing::ThrowsMessage<TimeoutError>("id_timeout timeout"))
+  EXPECT_THROW_WITH_MESSAGE(
+    (run_with_timeout("id_timeout",
+                      ms(100),
+                      [&]{ return id_timeout(ARG); })),
+    TimeoutError,
+    "id_timeout timeout")
       << "Function timeout yields exception.";
 
   auto endless_loop = [](flag done, flag aborted)
@@ -60,22 +78,20 @@ TEST(TimeoutTest, CanTimeoutFunction)
     throw AbortedError("endless_loop_abort");
   };
 
-  flag endless_loop_done(unset());
+  flag done(unset());
 
-  EXPECT_THAT(
-    [&]() {
-      run_abortable_with_timeout(
-        "endless_loop",
-        ms(100),
-        [&](flag aborted)
-        { return endless_loop(endless_loop_done, aborted); });
-    },
-    testing::ThrowsMessage<TimeoutError>("endless_loop timeout"))
+  EXPECT_THROW_WITH_MESSAGE(
+    run_abortable_with_timeout("endless_loop",
+                               ms(100),
+                               [&](flag aborted)
+                               { return endless_loop(done, aborted); }),
+    TimeoutError,
+    "endless_loop timeout")
       << "Function timeout yields exception.";
 
   sleep(ms(100));
 
-  EXPECT_TRUE(is_set(endless_loop_done))
+  EXPECT_TRUE(is_set(done))
     << "Timed out thread terminates execution after abort flag is set.";
 
   wait_for_timed_out_threads();
