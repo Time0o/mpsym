869cac8a03c4532e422affc8bccaf8c0edd783f2
Timo Nicolai <timonicolai@arcor.de>
2020-12-17 13:31:36 +0100

    pympsym -> mpsym
    Also adds mpsym.test()

---
+++ mpsym/__init__.py
@@ -0,0 +1,37 @@
+import os as _os
+import sys as _sys
+import unittest as _unittest
+
+from ._mpsym import *
+from . import _mpsym_tests
+
+
+def _silent(cmd):
+    _stdout = _sys.stdout
+    _stderr = _sys.stderr
+
+    try:
+        with open(_os.devnull, 'w') as devnull:
+            _sys.stdout = devnull
+            _sys.stderr = devnull
+
+            return cmd()
+
+    finally:
+        _sys.stdout = _stdout
+        _sys.stderr = _stderr
+
+
+def test(verbosity=-1):
+    def run_tests(verbosity=0):
+        suite = _unittest.TestLoader().loadTestsFromModule(_mpsym_tests)
+        runner = _unittest.TextTestRunner(verbosity=verbosity)
+
+        return runner.run(suite)
+
+    if verbosity == -1:
+        result = _silent(run_tests)
+    else:
+        result = run_tests(verbosity=verbosity)
+
+    return result.wasSuccessful()
