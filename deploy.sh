69a1e1103d6b9cf6aa0c43944e7081ff170b148c
Timo Nicolai <timonicolai@arcor.de>
2020-12-17 13:51:03 +0100

    Don't deploy to PyPi

--- deploy.sh
+++ deploy.sh
@@ -14,10 +14,6 @@ USER_NAME="Time0o"
 REPO_NAME="mpsym"
 REPO_URL="github.com/$USER_NAME/$REPO_NAME.git"
 
-PYPI_DOCKER_IMAGE=quay.io/pypa/manylinux2014_x86_64
-PYPI_DOCKER_SRC_DIR=/mpsym
-PYPI_DOCKER_WHEELS_DIR="$PYPI_DOCKER_SRC_DIR/wheelhouse"
-
 # upload coverage data
 echo "Uploading coverage data..."
 mkdir gcov
@@ -55,23 +51,3 @@ else
   echo "Failed to find generated documentation" >&2
   exit 1
 fi
-
-cd ..
-
-# publish PyPi package
-echo "Publishing PyPi package"
-
-docker pull "$PYPI_DOCKER_IMAGE"
-
-TWINE_REPOSITORY=pypi
-TWINE_USER_NAME="$USER_NAME"
-TWINE_PASSWORD="$TWINE_PYPI_PASSWORD"
-
-docker run --rm -v "$(dirname "$0")":"$PYPI_DOCKER_SRC_DIR" \
-  -e SRC_DIR="$PYPI_DOCKER_SRC_DIR" \
-  -e WHEELS_DIR="$PYPI_DOCKER_WHEELS_DIR" \
-  -e PIP_TESTPYPI="$PIP_TESTPYPI" \
-  -e TWINE_REPOSITORY="$TWINE_REPOSITORY" \
-  -e TWINE_USER_NAME="$TWINE_USER_NAME" \
-  -e TWINE_PASSWORD="$TWINE_PASSWORD" \
-  "$PYPI_DOCKER_IMAGE" "$PYPI_DOCKER_SRC_DIR/buildwheels.sh"
